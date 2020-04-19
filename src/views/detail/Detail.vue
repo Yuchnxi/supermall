5660a9d0acd2036067c0d1e1fef92b993f9eb4dd
yuchnxi <yuchenxi748521@163.com>
2020-04-20 00:35:17 +0800

    2020-4-20

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,9 +1,14 @@
 <template>
   <div id='detail'>
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
-    <detail-shop-info :shop="shop"></detail-shop-info>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
+    </scroll>
   </div>
 </template>
 
@@ -13,8 +18,13 @@ import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
 import DetailBaseInfo from './childComps/DetailBaseInfo'
 import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailParamInfo from './childComps/DetailParamInfo' 
+import DetailCommentInfo from './childComps/DetailCommentInfo'
 
-import {getDetail, Goods, Shop} from 'network/detail'
+import Scroll from 'components/common/scroll/Scroll'
+
+import {getDetail, Goods, Shop, GoodsParam} from 'network/detail'
 
 export default {
   name: 'Detail',
@@ -23,14 +33,21 @@ export default {
       iid: {},
       topImages: [],
       goods: {},
-      shop: {}
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {}
     }
   },
   components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
-    DetailShopInfo
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    Scroll
   },
   created(){
     // 1. 保存传入的 iid
@@ -48,12 +65,45 @@ export default {
 
         // 3. 创建店铺信息的对象
         this.shop = new Shop(data.shopInfo)
+
+        // 4. 保存店铺图片
+        this.detailInfo = data.detailInfo
+
+        // 5. 获取参数信息
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+        // 6.获取评论信息
+        if (data.rate.cRate !== 0) {
+          this.commentInfo = data.rate.list[0]
+        }
       })
   },
   mounted(){},
-  methods: {}
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
+  }
 }
 </script>
 <style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+
+  .detail-nav{
+    position: relative;
+    z-index: 9;
+    background: #fff;
+  }
+  .content {
+  overflow: hidden;
+  height: calc(100% - 44px);
+
+}
+
 
 </style>
